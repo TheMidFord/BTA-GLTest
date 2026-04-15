805d54465ef18eb731da524d011d19d52fa980c2
MidFord <71330862+TheMidFord@users.noreply.github.com>
2026-04-16 00:12:15 +0200

    Setup

--- src/main/java/malicedev/gltest/Main.java
+++ src/main/java/malicedev/gltest/Main.java
@@ -1,4 +1,4 @@
-package turniplabs.examplemod;
+package malicedev.gltest;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -6,12 +6,12 @@ import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
-	public static final String MOD_ID = "examplemod";
+public class Main implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+	public static final String MOD_ID = "gltest";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	@Override
 	public void onInitialize() {
-		LOGGER.info("ExampleMod initialized.");
+		LOGGER.info("GLTest initialized.");
 	}
 
 	@Override
